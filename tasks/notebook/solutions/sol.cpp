a6542141538f30e923f457b3bb8a0a20e80a6c0c
Dmitriy Klebanov <dmitriy.klebanov@gmail.com>
2019-02-05 19:47:37 +0300

    fixed notebook solution

--- tasks/notebook/solutions/sol.cpp
+++ tasks/notebook/solutions/sol.cpp
@@ -5,6 +5,23 @@
 
 using namespace std;
 
+int64_t solve(const string& s, int m) {
+	f[0].assign(m, 0);
+	f[1].assign(m, 0);
+	ans = 0;
+	for (size_t i = 0; i < s.size(); i++) {
+		for (int j = 0; j < m; j++) {
+			f[(i + 1) & 1][j] = 0;
+		}
+		f[(i + 1) & 1][(s[i] - '0') % m] = 1;
+		for (int j = 0; j < m; j++) {
+			f[(i + 1) & 1][(j * 10 + s[i] - '0') % m] += f[i & 1][j];
+		}
+		ans += f[(i + 1) & 1][0];
+	}
+	return ans;
+}
+
 int main() {
 	string s; int m;
 	cin >> s; m = 4; //cf edition
